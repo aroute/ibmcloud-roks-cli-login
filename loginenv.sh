22142169155441d262ac515797f30d37355b304f
Arif <aroute@users.noreply.github.com>
2021-11-18 07:38:10 -0600

    Create loginenv.sh

---
+++ loginenv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+## ITZ-V2 (TechZone Account)
+export IBMCLOUD_API_KEY=<my API key>
+export IBM_CLOUD_ACCOUNT=<BSS ID>
+export IBM_CLOUD_REGION=us-east
+export IBM_CLOUD_OPENSHIFT_CLUSTER_ID=<Cluster ID>
+
+
+# IBM Cloud Regions
+# https://cloud.ibm.com/docs/containers?topic=containers-regions-and-zones#zones-mz
+# 1. au-syd
+# 2. in-che
+# 3. jp-osa
+# 4. jp-tok
+# 5. kr-seo
+# 6. eu-de
+# 7. eu-gb
+# 8. ca-tor
+# 9. us-south
+# 10. us-south-test
+# 11. us-east
+# 12. br-sao
