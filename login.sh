ff752916999656c49efde583227ff0a4aab755f3
Arif <aroute@users.noreply.github.com>
2021-11-18 07:54:46 -0600

    Update login.sh

--- login.sh
+++ login.sh
@@ -4,4 +4,4 @@ source $(dirname $(realpath ${0}))/loginenv.sh
 
 ibmcloud login --apikey "${IBMCLOUD_APIKEY}" -c "${IBMCLOUD_ACCOUNT}" -r "${IBMCLOUD_REGION}"
 ibmcloud oc cluster config -c "${IBMCLOUD_OCP_CLUSTERID}"
-oc login -u apikey -p "${IBMCLOUD_API_KEY}"
+oc login -u apikey -p "${IBMCLOUD_APIKEY}"
