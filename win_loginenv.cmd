94bf37a96ea506ae4b9b6df176e26b292e11a319
Arif <aroute@users.noreply.github.com>
2022-02-24 06:30:09 -0600

    Adding environment file for Windows.

---
+++ win_loginenv.cmd
@@ -0,0 +1,23 @@
+(
+ENDLOCAL
+set IBMCLOUD_APIKEY=<my API key>
+set IBMCLOUD_ACCOUNT=<BSS ID>
+set IBMCLOUD_REGION=us-east
+set IBMCLOUD_OCP_CLUSTERID=<Cluster ID>
+EXIT /b 0
+)
+
+REM IBM Cloud Regions
+REM https://cloud.ibm.com/docs/containers?topic=containers-regions-and-zonesREMzones-mz
+REM 1. au-syd
+REM 2. in-che
+REM 3. jp-osa
+REM 4. jp-tok
+REM 5. kr-seo
+REM 6. eu-de
+REM 7. eu-gb
+REM 8. ca-tor
+REM 9. us-south
+REM 10. us-south-test
+REM 11. us-east
+REM 12. br-sao
